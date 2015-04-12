d9bed765cf1e06e9050952c0b2334e7fd4a788fa
FanWuyou <fanwuyou@qq.com>
2015-04-13 06:14:41 +0800

    Revert "have done"
    This reverts commit cbf1227c0dba28dba4f6a6eccbbe2ee4b94a08ae.

--- plot1.R
+++
@@ -1,8 +0,0 @@
-a<-read.table("./a/household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?",nrows=300000)
-b<- a[(a$Date == "1/2/2007"), ]
-c<- a[(a$Date == "2/2/2007"), ]
-d<-rbind(b,c)
-hist(d[,"Global_active_power"],breaks = 24,col = "red",main="Global active power",xlab="Global active power(kiolowatts)")
-library(datasets)
-dev.copy(png,file="plot1.png")
-dev.off()
